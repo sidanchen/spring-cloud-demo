b7f092f6b269f26b2c6821ea417d8d07a4046914
Chensidan <1536242521@qq.com>
2018-08-12 20:06:16 +0800

    3.添加ribbon客户端负载均衡工具

---
+++ spring-cloud-ribbon/src/main/java/com/csd/SpringCloudRibbonApplication.java
@@ -0,0 +1,12 @@
+package com.csd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRibbonApplication.class, args);
+	}
+}
