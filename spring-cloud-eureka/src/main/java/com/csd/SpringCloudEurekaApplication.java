1c6a550d823009dce5356fa553b0abcb79d90937
Chensidan <1536242521@qq.com>
2018-08-12 20:30:25 +0800

    update eureka module

--- spring-cloud-eureka/src/main/java/com/csd/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/csd/SpringCloudEurekaApplication.java
@@ -2,8 +2,11 @@ package com.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//注解启动eureka支持
+@EnableEurekaServer
 public class SpringCloudEurekaApplication {
 
 	public static void main(String[] args) {
