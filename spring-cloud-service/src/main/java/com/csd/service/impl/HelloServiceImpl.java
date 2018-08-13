43a99609f04bb5dbc7bcef04b539032acf2482b4
Chensidan <1536242521@qq.com>
2018-08-13 11:58:21 +0800

    服务提供者添加随机数 使线程随机睡眠0-7秒 保证随机触发服务器容错保护机制

--- spring-cloud-service/src/main/java/com/csd/service/impl/HelloServiceImpl.java
+++ spring-cloud-service/src/main/java/com/csd/service/impl/HelloServiceImpl.java
@@ -3,6 +3,8 @@ package com.csd.service.impl;
 import com.csd.service.HelloService;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 /**
  * Created by sdc on 2018/8/12.
  */
@@ -10,6 +12,13 @@ import org.springframework.stereotype.Service;
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello() {
+        int sl = new Random().nextInt(7);
+        try {
+            System.out.println(sl*1000);
+            Thread.sleep(sl*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "hello sprigncloud";
     }
 }
