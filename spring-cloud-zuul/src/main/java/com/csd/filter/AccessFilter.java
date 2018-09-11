a662eba62526c51af23b1d422e6141dec2e50b49
Dzh <15986616910@163.com>
2018-09-11 14:33:30 +0800

    添加拦截代码

--- spring-cloud-zuul/src/main/java/com/csd/filter/AccessFilter.java
+++ spring-cloud-zuul/src/main/java/com/csd/filter/AccessFilter.java
@@ -1,7 +1,10 @@
 package com.csd.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class AccessFilter extends ZuulFilter{
     @Override
@@ -21,7 +24,15 @@ public class AccessFilter extends ZuulFilter{
 
     @Override
     public Object run() throws ZuulException {
-        System.out.println("进行权限拦截了！");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String accessToken = request.getParameter("accessToken");
+        if(accessToken == null || !accessToken.equals("123")){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            ctx.getResponse().setCharacterEncoding("GBK");
+            ctx.setResponseBody("权限不足！");
+        }
         return null;
     }
 }
